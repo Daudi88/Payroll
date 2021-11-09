305e43ff6316fd422cc737c774fea24c5ce31de0
Daudi88 <david.strom88@gmail.com>
2021-11-09 10:16:11 +0100

    Add Login method

---
+++ Payroll/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Payroll.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll.Controllers
+{
+    public class LoginController
+    {
+        public bool Login(string username, string password)
+        {
+            var db = new PayrollContext();
+            var account = db.Accounts.FirstOrDefault(a => a.Username == username && a.Password == password);
+            if (account != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ PayrollTests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payroll.Controllers;
+using Payroll.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll.Controllers.Tests
+{
+    [TestClass()]
+    public class LoginControllerTests
+    {
+        [TestMethod()]
+        [DataRow("admin1", "admin1234", true)]
+        [DataRow("admin1", "admin123", false)]
+        [DataRow("admin", "admin1234", false)]
+        [DataRow("", "admin1234", false)]
+        [DataRow("admin1", "", false)]
+        [DataRow(null, "admin1234", false)]
+        [DataRow("admin1", null, false)]
+        public void LoginTest(string username, string password, bool expected)
+        {
+            var loginController = new LoginController();
+            var actual = loginController.Login(username, password);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
