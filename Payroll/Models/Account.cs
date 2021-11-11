41f07abe3c7cf4c109dfd7e9a56a61c4f771c753
Daudi88 <david.strom88@gmail.com>
2021-11-11 15:18:14 +0100

    Replace database for .txt-file

--- Payroll/Models/Account.cs
+++ Payroll/Models/Account.cs
@@ -1,15 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Payroll.Models
+﻿namespace Payroll.Models
 {
-    public class Account
+    public abstract class Account
     {
-        [Key]
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public decimal Salary { get; set; }
-        public Role Role { get; set; }
+        public string Role { get; set; }
         public bool IsAdmin { get; set; }
     }
 }
--- Payroll/Services/Seeder.cs
+++ Payroll/Services/Seeder.cs
@@ -1,36 +1,60 @@
 ﻿using Payroll.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Payroll.Services
 {
-    class Seeder
+    public class Seeder
     {
+        public static List<Account> Accounts = new();
+
         public static void Seed()
         {
-            using var db = new PayrollContext();
-            if (!db.Roles.Any())
+            var fileName = @"..\..\..\..\accounts.txt";
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+            var input = File.ReadAllLines(path);
+            
+            foreach (var line in input)
             {
-                db.Roles.AddRange(new List<Role>
+                var inputLine = line.Split(',');
+
+                // Checks IsAdmin to determine what type of account to create.
+                if (inputLine[5] == "true")
                 {
-                    new Role{Description="Cheif Excecutive Officer"},
-                    new Role{Description="Cheif Technology Officer"},
-                    new Role{Description="Marketing Manager"},
-                    new Role{Description="Product Manager"},
-                    new Role{Description="Project Manager"},
-                    new Role{Description="Business Analyst"},
-                    new Role{Description="Software Engineer"},
-                    new Role{Description="Fullstack Developer"},
+                    var account = new Admin
+                    {
+                        Id = int.Parse(inputLine[0]),
+                        Username = inputLine[1],
+                        Password = inputLine[2],
+                        Salary = int.Parse(inputLine[3]),
+                        Role = inputLine[4],
+                    };
 
-                });
+                    Accounts.Add(account);
+                }
+                else
+                {
+                    var account = new User
+                    {
+                        Id = int.Parse(inputLine[0]),
+                        Username = inputLine[1],
+                        Password = inputLine[2],
+                        Salary = int.Parse(inputLine[3]),
+                        Role = inputLine[4],
+                    };
 
-                db.SaveChanges();
+                    Accounts.Add(account);
+                }
             }
 
-            if (!db.Accounts.Any())
+            if (Accounts.Count < 1)
             {
-                db.Accounts.Add(new Admin { Username = "admin1", Password = "admin1234", Role = db.Roles.FirstOrDefault(r => r.Description == "Cheif Excecutive Officer"), Salary = 50000});
-                db.SaveChanges();
+                var admin = new Admin { Id = 1, Username = "admin1", Password = "admin1234", Salary = 50000, Role = "Cheif Excecutive Officer" };
+                Accounts.Add(admin);
+                var content = new string[] { $"{admin.Id},{admin.Username},{admin.Password},{admin.Salary},{admin.Role},{admin.IsAdmin}," };
+                File.WriteAllLines(path, content);
             }
         }
     }
