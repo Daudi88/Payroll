8654dea94880dc27199ccb2d399e346ffa78b2a6
Emma-LinLin <74680845+Emma-LinLin@users.noreply.github.com>
2021-11-12 14:57:10 +0100

    Create Add and Remove method, added tests

---
+++ Payroll/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Payroll.Models;
+using Payroll.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll.Controllers
+{
+    public class AccountController
+    {
+        public bool Add(Account newAccount)
+        {
+            if(!string.IsNullOrEmpty(newAccount.Username))
+            {
+                foreach(var account in Database.Accounts)
+                {
+                    if(newAccount.Username == account.Username)
+                    {
+                        return false;
+                    }
+                }
+
+                if(!string.IsNullOrEmpty(newAccount.Password))
+                {
+                    Database.Accounts.Add(newAccount);
+                    Database.Save();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Remove(Account account)
+        {
+            try
+            {
+                Database.Accounts.Remove(account);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PayrollTests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payroll.Controllers;
+using Payroll.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll.Controllers.Tests
+{
+    [TestClass()]
+    public class AccountControllerTests
+    {
+        User validUser;
+        User invalidUser;
+        User existingUsername;
+        AccountController accountCont;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            validUser = new User {Username = "user1", Password = "user1234" };
+            invalidUser = new User {Username = "   " };
+            existingUsername = new User { Username = "admin1" };
+            accountCont = new AccountController();
+        }
+
+        [TestMethod()]
+        public void AddAndRemoveTest_ValidUser_ReturnsTrue()
+        {
+            var actual1 = accountCont.Add(validUser);
+            Assert.IsTrue(actual1);
+            var actual2 = accountCont.Remove(validUser);
+            Assert.IsTrue(actual2);
+        }
+
+        [TestMethod()]
+        public void AddTest_InvalidUser_ReturnsFalse()
+        {
+            var actual = accountCont.Add(invalidUser);
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void AddTest_ExistingUser_ReturnsFalse()
+        {
+            var actual = accountCont.Add(existingUsername);
+            Assert.IsFalse(actual);
+        }
+    }
+}
